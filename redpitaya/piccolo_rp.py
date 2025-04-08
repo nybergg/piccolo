ed119c388425fea30359f5938d5b7e30c480c5bb
nybergg <kendra.nyberg@gmail.com>
2025-04-08 15:24:05 -0700

    piccolo_instrument.py: - modified print statement to clearly indicate     the output is coming from the Red Pitaya piccolo_rp.py: - cleaned up mmap_info reading, formatting, etc - mmap_info reading and formatting works! - added a lookup dict for mmap_info for easier read/write capabilities - cleaned up write functions to use the new mmap_lookup dict - write functions work!

--- redpitaya/piccolo_rp.py
+++ redpitaya/piccolo_rp.py
@@ -15,65 +15,83 @@ class PiccoloRP:
         self.verbose = verbose
         self.very_verbose = very_verbose
         self._map_memory()
+        self._get_mmap_info()
     
-    ##### Memory reading and writing methods #####
+    ##### Memory mapping methods #####
     def _map_memory(self):
-        """Map the memory addresses from the FADS memory region to the FADS variables."""
+        """Map the memory map region."""
         base_addr = 0x40600000
         map_size = 0x2000  # 8 KB mapping
         
+        # Debugging
+        if self.verbose:
+            print("--------Mapping memory region--------")
+        if self.very_verbose:
+            print(f"Memory mapping at address {hex(base_addr)} with size {map_size} bytes...")
+
         # Create a memory mapping of the FADS memory region
         mem_fd = open("/dev/mem", "r+b")
         self.mmap = mmap.mmap(mem_fd.fileno(), map_size, mmap.MAP_SHARED,
                                 mmap.PROT_READ | mmap.PROT_WRITE, offset=base_addr)
         
-        # Get FADS variable information and adjust formatting
-        self._get_mmap_info()
-        self._expand_mmap_info()
-        self._interpret_datatypes()
-        
         # Debugging
         if self.verbose:
-            print("Memory map completed.")
-        if self.very_verbose:
-            print("Mapped Parameters:")
-            for var in self.mmap_info:
-                print(var)
-
+            print("Done mapping memory.")
+        
         return None
     
     def _get_mmap_info(self):
-        """Get information about the piccolo variables and their memory addresses."""
+        """Get information about the Piccolo variables and format for easy read/write."""
+        # Debugging
+        if self.verbose:
+            print("--------Loading memory map information--------")
+        
+        # Load the JSON file containing the memory map information
         script_dir = os.path.dirname(os.path.realpath(__file__))
         json_path = os.path.join(script_dir, "piccolo_mmap.json")
         with open(json_path, "r") as f:
             mmap_json = json.load(f)
 
-        # Assign the piccolo parameter information to the class attributes
-        self.fads_parameters_info = mmap_json["fads_parameters"]
-        self.droplet_parameters_info = mmap_json["droplet_parameters"]
-        self.sort_gates_info = mmap_json["sort_gates"]
+        # Extract paremeter information for FADS, droplet, and sort gates
+        fads_parameters = mmap_json["fads_parameters"]
+        droplet_parameters = mmap_json["droplet_parameters"]
+        sort_gates = mmap_json["sort_gates"]
 
         # Join the parameter information into a single list
         self.mmap_info = (
-            self.fads_parameters_info
-            + self.droplet_parameters_info
-            + self.sort_gates_info
+            fads_parameters
+            + droplet_parameters
+            + sort_gates
         )
+
+        # Format the mmap_info for easy read/write
+        self._expand_mmap_info()
+        self._interpret_mmap_dtypes()
+        
+        # Create an mmap dictionary for easy lookup by variable name
+        self.mmap_lookup = {var["name"]: var for var in self.mmap_info}
+        
+        # Store a list of variable names for FADS, droplet, and sort gates
+        self.fads_parameter_names = [v["name"] for v in fads_parameters]
+        self.droplet_parameter_names = [v["name"] for v in droplet_parameters]
+        self.sort_gate_names = [v["name"] for v in sort_gates]
 
         # Debugging
         if self.verbose:
-            print("Memory map information loaded.")
+            print("Memory map information loaded and formatted.")
         if self.very_verbose:
-            print("Mapped Parameters:")
-            for param in self.fads_parameters_info:
+            print("Created a lookup dict for mmap info with keys:")
+            for param in self.mmap_lookup:
                 print(param)
-            print("Droplet Parameters:")
-            for param in self.droplet_parameters_info:
+            print("List of FADS Parameters:")
+            for param in self.fads_parameter_names:
                 print(param)
-            print("Sort Gates:")
-            for gate in self.sort_gates_info:
-                print(gate)
+            print("List of Droplet Parameters:")
+            for param in self.droplet_parameter_names:
+                print(param)
+            print("List of Sort Gates:")
+            for param in self.sort_gate_names:
+                print(param)
 
         return None
     
@@ -109,7 +127,7 @@ class PiccoloRP:
 
         return None
 
-    def _interpret_datatypes(self):
+    def _interpret_mmap_dtypes(self):
         """Interpret the Verilog datatypes in the JSON file and convert them to Python datatypes."""
         dtype_intepretter = {
             "32'd":  { "fmt": "<I", "bits": 32, "signed": False, "bool": False },
@@ -129,7 +147,7 @@ class PiccoloRP:
             else:
                 raise ValueError(f"Unsupported data type: {dtype}")
             
-        # Debugging
+        # Debug
         if self.verbose:
             print("Data types interpreted.")
         if self.very_verbose:
@@ -140,37 +158,69 @@ class PiccoloRP:
         return None
     
 
-
-    def _read_memory_value(self, address, dtype, size, mask_int=None):
-        """Helper function to read a single memory value from the specified address."""
-        offset = int(address, 16)
-        self.mem_map.seek(offset)
-        data = self.mem_map.read(size)
-        if len(data) != size:
-            self.logger.error(f"Failed to read {size} bytes from address {address}.")
-            return None
-        value = struct.unpack(dtype, data)[0]
-        if mask_int is not None:
-            value = value & mask_int
-            # For 14-bit registers (mask 0x3FFF), perform sign extension.
-            if mask_int == 0x3FFF:
-                if value & (1 << 13):
-                    value = value - (1 << 14)
-        return value
-    
-    def read_memory(self, addr, dtype, size, mask_int=None):
-        """
-        Reads from memory using the precomputed size and mask.
-        If addr is a list, reads each address separately and returns a list of values.
-        """
-        if isinstance(addr, list):
-            values = []
-            for a in addr:
-                values.append(self._read_memory_value(a, dtype, size, mask_int))
-            return values
-        else:
-            return self._read_memory_value(addr, dtype, size, mask_int)
+    ##### Memory reading and writing methods #####
+    def read_memory_all(self):
+        """Read all variables from mmap_info."""
+        if self.verbose:
+            print("--------Reading all variables from memory map--------")
         
+        all_values = {}
+        for var in self.mmap_info:
+            var_name = var["name"]
+            val = self.read_memory_var(var_name)
+            all_values[var["name"]] = val            
+        
+        self.all_values = all_values
+
+        # Debug
+        if self.verbose:
+            print("Done reading all variables from memory mapped region.")
+        if self.very_verbose:
+            print("All variables read from memory mapped region:")
+            for var_name, val in all_values.items():
+                print(f"{var_name}: {val}")
+        
+        return None
+    
+    def read_memory_var(self, var_name):
+        """Read a memory value from the specified address."""
+        var_conf = self.mmap_lookup.get(var_name)
+
+        if var_conf is None:
+            raise ValueError(f"Variable {var_name} not found in Piccolo variables.")
+
+        offset = int(var_conf["addr"], 16) if isinstance(var_conf["addr"], str) else var_conf["addr"]
+        fmt = var_conf["fmt"]
+        bits = var_conf["bits"]
+        is_signed = var_conf["signed"]
+        is_bool = var_conf["bool"]
+        
+        # Get the address and offset
+        self.mmap.seek(offset)
+        data = self.mmap.read(4) # Read block is always 4 bytes on RP
+        
+        # Determine the minimal size required for this format
+        var_size = struct.calcsize(fmt)
+
+        # Use only the variable-specific bytes from the 4-byte block.
+        var_data = data[:var_size]
+        val = struct.unpack(fmt, var_data)[0]
+
+        # If smaller than the struct size, mask and sign-extend if needed
+        actual_bits = var_size * 8
+        if bits < actual_bits: 
+            mask = (1 << bits) - 1 # e.g., for 14 bits, mask = 0x3FFF
+            val &= mask
+            if is_signed:
+                sign_bit = 1 << (bits - 1)
+                if val & sign_bit:
+                    val -= (1 << bits)
+
+        if is_bool:
+            val = bin(val)[2:] #convert to binary string  
+        
+        return val
+
 
     ##### Logging methods #####
     def update_logging(self):
@@ -333,4 +383,5 @@ if __name__ == "__main__":
     args = parser.parse_args()
     
     rp = PiccoloRP(verbose=args.verbose, very_verbose=args.very_verbose)
+    rp.read_memory_all()
 
