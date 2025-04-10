3e9c49c8f9d878f1d34d3d2f74fe80af0009f19f
Alfred Millett-Sikking <amsikking@gmail.com>
2025-04-10 10:10:58 -0700

    Update piccolo_instrument_sim.py
    Tidied up data attribute into signal attribute.

--- piccolo_instrument_sim.py
+++ piccolo_instrument_sim.py
@@ -34,16 +34,14 @@ class DataGenerator:
             print("%s: opening..."%self.name)
         self.time_ms = np.arange(
             0, self.signal_duration_ms, self.sampling_interval_ms)
+        self.signal = [np.zeros_like(self.time_ms)] * num_channels
         self.drop_arrival_time_ms = np.arange(
             0, self.signal_duration_ms, self.drop_interval_ms)
-        
         self.set_threshold(0.03)
         self.set_gate_limits({"x0": 0, "y0": 0, "x1": 0, "y1": 0})
         self.pmt_gain = np.zeros(num_channels)
         for ch in range(num_channels):
             self.set_pmt_gain(ch, 0.5)
-        self.data = {"pmt0": {"x": [0], "y": [0]},
-                     "pmt1": {"x": [0], "y": [0]}}
         self.data2d = {"x": [0], "y": [0], "density": [0]}
         self._running = False
         if self.verbose:
@@ -77,8 +75,7 @@ class DataGenerator:
                                               scale=self.signal_baseline_cv,
                                               size=len(self.time_ms))            
             # Combine signals for this channel:
-            signal = (signal + baseline_noise) * self.pmt_gain[ch]
-            self.data[f"pmt{ch}"] = {"x": self.time_ms, "y": signal}
+            self.signal[ch] = (signal + baseline_noise) * self.pmt_gain[ch]
         if self.very_verbose:
             print("\n%s: -> done generating signal"%self.name)            
         return None
@@ -88,14 +85,13 @@ class DataGenerator:
             print("\n%s: analyzing drops"%self.name)
         # Analyze Drop Parameters from PMT Signals:
         # Find drops based on the signal and threshold of the specified channel:
-        detection_signal = self.data[f"pmt{ch}"]["y"]
-        drops, _ = find_peaks(detection_signal, height=self.threshold)
+        drops, _ = find_peaks(self.signal[ch], height=self.threshold)
         if np.any(drops) == False:
             print('No peaks detected in reference channel')
         else:
             # Calculate fwhm of peaks to define time range for each drop:
             widths, _, left_ips, right_ips = peak_widths(
-                detection_signal, drops, rel_height=0.5)
+                self.signal[ch], drops, rel_height=0.5)
             # Convert widths to time units:
             drop_widths = widths * self.sampling_interval_ms  
             # Filter drops based on width constraints:
@@ -131,20 +127,18 @@ class DataGenerator:
                     zip(valid_left_ips, valid_right_ips, valid_drop_widths),
                     start=1):
                     for ch in range(self.num_channels):
-                        # Specify the signal from a given channel:
-                        channel_signal = self.data[f"pmt{ch}"]["y"]
                         # Isolate baseline signal by excluding drop indices:
                         # (technically don't need to do this for every drop)
                         baseline_indices = np.setdiff1d(
-                            np.arange(len(channel_signal)), excluded_indices)
+                            np.arange(len(self.signal[ch])), excluded_indices)
                         baseline_signals[ch] = np.median(
-                            channel_signal[baseline_indices])
+                            self.signal[ch][baseline_indices])
                         baseline = np.mean(baseline_signals[ch])
                         # Isolate drop signal:
-                        drop_signal = channel_signal[int(left) : int(right)]
+                        drop_signal = self.signal[ch][int(left) : int(right)]
                         # Calculate drop parameters:
                         max_signal = drop_signal.max()
-                        drop_time = self.data[f"pmt{ch}"]["x"][int(left)]
+                        drop_time = self.time_ms[int(left)]
                         auc = simpson(drop_signal, dx=self.sampling_interval_ms)
                         fwhm = width
                         drop_width = (right - left) * self.sampling_interval_ms
